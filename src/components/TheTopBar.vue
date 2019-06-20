dce27ea652aeecc2b6d194b834420760133416cf
myuon <ioi.joi.koi.loi@gmail.com>
2019-06-21 02:17:58 +0900

    refactor: TopBar

--- src/components/TheTopBar.vue
+++ src/components/TheTopBar.vue
@@ -1,44 +1,27 @@
 <template>
-  <v-toolbar
-    absolute
-    app
-    clipped-left
-    dense
-    flat
-    dark
-    class="indigo darken-1"
-  >
-    <v-toolbar-side-icon
-      @click.stop="toggleDrawer"
-    ></v-toolbar-side-icon>
-    <v-toolbar-title><router-link to="/dashboard" style="color: #fff; text-decoration: none;">Portals@me</router-link></v-toolbar-title>
+  <v-toolbar absolute app clipped-left dense flat dark class="indigo darken-1">
+    <v-toolbar-side-icon @click.stop="toggleDrawer"></v-toolbar-side-icon>
+    <v-toolbar-title>
+      <router-link to="/dashboard" style="color: #fff; text-decoration: none;">Portals@me</router-link>
+    </v-toolbar-title>
 
     <v-spacer></v-spacer>
 
-    <v-chip color="purple lighten-2" small v-if="$config.isDev">
-      Development
-    </v-chip>
+    <v-chip color="purple lighten-2" small v-if="$config.isDev">Development</v-chip>
 
     <v-toolbar-items v-if="user != null">
       <v-menu offset-y>
-        <v-btn
-          slot="activator"
-          flat
-        >
+        <v-btn slot="activator" flat>
           <v-avatar color="orange" size="32px">
-            <v-img :src="user.picture" />
+            <v-img :src="user.picture"/>
           </v-avatar>
           &nbsp;&nbsp;{{ user.display_name }}
         </v-btn>
         <v-list>
-          <v-list-tile
-            @click="$router.push(`/users/${user.name}`)"
-          >
+          <v-list-tile @click="$router.push(`/users/${user.user_name}`)">
             <v-list-tile-title>プロフィール</v-list-tile-title>
           </v-list-tile>
-          <v-list-tile
-            @click="signOut"
-          >
+          <v-list-tile @click="signOut">
             <v-list-tile-title>サインアウト</v-list-tile-title>
           </v-list-tile>
         </v-list>
@@ -48,41 +31,39 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { User } from '../views/User.vue';
+import Vue from "vue";
+import { User } from "../views/User.vue";
 
 export default Vue.extend({
-  data () {
+  data() {
     return {
-      user: null as User | null,
+      user: null as User | null
     };
   },
   methods: {
-    async signOut () {
-      localStorage.setItem('id_token', '');
-      localStorage.setItem('user', '{}');
+    async signOut() {
+      localStorage.setItem("id_token", "");
+      localStorage.setItem("user", "{}");
       this.user = null;
-      this.$router.push('/');
+      this.$router.push("/");
     },
-    toggleDrawer () {
-      this.$store.commit('toggleDrawer')
+    toggleDrawer() {
+      this.$store.commit("toggleDrawer");
     }
   },
-  async mounted () {
+  async mounted() {
     try {
-      this.user = JSON.parse(localStorage.getItem('user') || '');
+      this.user = JSON.parse(localStorage.getItem("user") || "");
     } catch (e) {
       console.error(e);
       this.user = null;
     }
 
-    const needAuth = (str: string) => str.match(
-      /^\/collections\/(.*)/
-    );
+    const needAuth = (str: string) => str.match(/^\/collections\/(.*)/);
 
     if (!this.user && !needAuth(this.$route.path)) {
-      this.$router.push('/');
+      this.$router.push("/");
     }
-  },
-})
+  }
+});
 </script>
