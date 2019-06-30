858585bd95522af2f38211ece56a11ae7ed683f0
myuon <ioi.joi.koi.loi@gmail.com>
2019-06-30 16:22:24 +0900

    design: UserProfile

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,87 +1,24 @@
 <template>
-  <v-container>
+  <div class="container">
     <amplify-connect :query="getUser">
       <template slot-scope="{loading, data, errors}">
         <template v-if="loading">loading...</template>
         <template v-if="errors.length > 0">{{ errors }}</template>
         <v-layout justify-center v-if="data.getUserByName">
-          <v-flex xs8>
-            <v-avatar size="120" color="grey lighten-4">
-              <img :src="data.getUserByName.picture" alt="avatar">
-            </v-avatar>
-
-            <h2>{{ data.getUserByName.display_name }}</h2>
-            <p>@{{ data.getUserByName.name }}</p>
-
-            <v-dialog v-model="editDialog" width="500" v-if="data.getUserByName.id == me.id">
-              <v-btn outline color="indigo" style="margin-left: 0;" slot="activator">プロフィールを編集</v-btn>
-
-              <v-card>
-                <v-card-title>
-                  <edit-user-basic-profile
-                    :formData="{ name: data.getUserByName.name, display_name: data.getUserByName.display_name, picture: data.getUserByName.picture }"
-                    @submit="updateUserProfile"
-                  />
-                </v-card-title>
-              </v-card>
-            </v-dialog>
-
-            <v-btn
-              outline
-              color="indigo"
-              style="margin-left: 0;"
-              @click="follow"
-              v-if="data.getUserByName.id != me.id"
-            >このユーザーをフォロー</v-btn>
-
-            <v-flex xs12>
-              <v-list>
-                ユーザーアクティビティ
-                <template v-for="(item, index) in feed">
-                  <v-divider :key="'div-before-' + index" v-if="index == 0"/>
-
-                  <v-list-tile :key="index" two-line>
-                    <v-list-tile-content>
-                      <v-list-tile-title v-if="item.event_name == 'INSERT_COLLECTION'">
-                        コレクション
-                        <a
-                          @click="$router.push(`/collections/${item.item_id.split('collection##')[1]}`)"
-                        >{{ item.entity.title }}</a>を作りました
-                      </v-list-tile-title>
-                      <v-list-tile-title v-if="item.event_name == 'INSERT_ARTICLE'">
-                        作品
-                        <a
-                          @click="$router.push(`/collections/${item.item_id.split('/')[0].split('collection##')[1]}`)"
-                        >{{ item.entity.title }}</a>を投稿しました
-                      </v-list-tile-title>
-                      <v-list-tile-sub-title
-                        v-if="item.entity.description"
-                      >{{ item.entity.description }}</v-list-tile-sub-title>
-                    </v-list-tile-content>
-
-                    <v-list-tile-action>
-                      <v-list-tile-action-text>{{ new Date(item.timestamp * 1000).toLocaleString() }}</v-list-tile-action-text>
-                    </v-list-tile-action>
-                  </v-list-tile>
-
-                  <v-divider :key="'div-' + index"/>
-                </template>
-              </v-list>
-            </v-flex>
-          </v-flex>
+          <user-profile :user="data.getUserByName"/>
         </v-layout>
       </template>
     </amplify-connect>
-  </v-container>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import VueRouter from "vue-router";
 import sdk from "../app/sdk";
-import EditUserBasicProfile from "../components/EditUserBasicProfile.vue";
 import * as API from "../API";
 import * as queries from "../graphql/queries";
+import UserProfile from "./user/UserProfile.vue";
 
 export interface User {
   id: string;
@@ -89,7 +26,7 @@ export interface User {
 
 export default Vue.extend({
   components: {
-    EditUserBasicProfile
+    UserProfile
   },
 
   data() {
@@ -109,8 +46,6 @@ export default Vue.extend({
     }
   },
 
-  methods: {
-    updateUserProfile() {}
-  }
+  methods: {}
 });
 </script>
---
+++ src/views/user/UserProfile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="one-half is-centered">
+    <figure class="image is-128x128">
+      <img class="is-rounded" :src="user.picture">
+    </figure>
+
+    <h2>{{ user.display_name }}</h2>
+    <p>@{{ user.name }}</p>
+
+    <b-button type="is-primary">プロフィールを編集</b-button>
+    <b-button @click="follow" v-if="user.id != me.id">このユーザーをフォロー</b-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["user"],
+
+  data() {
+    return {
+      me: JSON.parse(localStorage.getItem("user")),
+      editDialog: false
+    };
+  },
+
+  methods: {
+    updateUserProfile() {},
+    follow() {}
+  }
+});
+</script>
+
