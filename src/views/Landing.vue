bbedce5871c3caf239b2c7d5b216ef0a69eb3cf3
myuon <ioi.joi.koi.loi@gmail.com>
2019-06-30 14:13:47 +0900

    design: Landing with Buefy

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,52 +1,56 @@
 <template>
-  <v-container grid-list-xl>
-    <v-layout row wrap text-xs-center>
-      <v-flex xs4 offset-xs4>
-        <h1>Portals@me</h1>
-        <p>クリエイターの発信の場を提供します</p>
-      </v-flex>
-    </v-layout>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Portals@me</h1>
+          <h2 class="subtitle">クリエイターの発信の場を提供します</h2>
+        </div>
+      </div>
+    </section>
 
-    <v-layout row wrap>
-      <v-flex xs3>
-        <v-icon x-large color="pink">share</v-icon>
-        <h2>作品の共有</h2>
-        <span>Portals@meから、あるいは他のサイトから作品を共有します。創作物はイラスト・文章・動画・ゲーム・ソフトウェアなど多岐にわたります。</span>
-      </v-flex>
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <b-icon icon="share-variant" size="is-large" style="color: #e16;"/>
+          <h2>作品の共有</h2>
+          <span>Portals@meから、あるいは他のサイトから作品を共有します。創作物はイラスト・文章・動画・ゲーム・ソフトウェアなど多岐にわたります。</span>
+        </div>
 
-      <v-flex xs3>
-        <v-icon x-large color="orange">insert_emoticon</v-icon>
-        <h2>リアクション</h2>
-        <span>素敵だと思った作品に対してはコメントやリアクションなどを付けてみましょう。作品への感想は新たな創作のきっかけとなります。</span>
-      </v-flex>
+        <div class="column">
+          <b-icon icon="emoticon" size="is-large" style="color: #f90;"/>
+          <h2>リアクション</h2>
+          <span>素敵だと思った作品に対してはコメントやリアクションなどを付けてみましょう。作品への感想は新たな創作のきっかけとなります。</span>
+        </div>
 
-      <v-flex xs3>
-        <v-icon x-large color="teal">collections</v-icon>
-        <h2>コレクション</h2>
-        <span>クリエイターの作品群はコレクションとして整理され公開されます。クリエイターのマルチな創作を支援し気軽な発信を手助けします。</span>
-      </v-flex>
+        <div class="column">
+          <b-icon icon="folder-multiple-image" size="is-large" style="color: #098;"/>
+          <h2>コレクション</h2>
+          <span>クリエイターの作品群はコレクションとして整理され公開されます。クリエイターのマルチな創作を支援し気軽な発信を手助けします。</span>
+        </div>
 
-      <v-flex xs3>
-        <v-icon x-large color="indigo">public</v-icon>
-        <h2>ソーシャル</h2>
-        <span>他のクリエイターの活動を覗いてみましょう。新たな作品の発見だけでなく、皆がどんなことに興味を持っているかも分かるかもしれません。</span>
-      </v-flex>
-    </v-layout>
+        <div class="column">
+          <b-icon icon="earth" size="is-large" style="color: #35b;"/>
+          <h2>ソーシャル</h2>
+          <span>他のクリエイターの活動を覗いてみましょう。新たな作品の発見だけでなく、皆がどんなことに興味を持っているかも分かるかもしれません。</span>
+        </div>
+      </div>
+    </div>
 
-    <v-layout row wrap text-xs-center>
-      <v-container>
-        <v-flex xs12>
-          <v-btn color="indigo" @click="$router.push('/signup')" dark>アカウントを作成</v-btn>
-          <p>あるいは <a @click="$router.push('/signin')">すでにアカウントを持っていますか？</a></p>
-        </v-flex>
-      </v-container>
-    </v-layout>
-  </v-container>
+    <div class="columns is-centered">
+      <div class="column is-half" style="text-align: center;">
+        <b-button size="is-medium" type="is-primary" @click="$router.push('/signup')" dark>アカウントを作成</b-button>
+        <p>
+          あるいは
+          <a @click="$router.push('/signin')">すでにアカウントを持っていますか？</a>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
-export default Vue.extend({
-})
+export default Vue.extend({});
 </script>
