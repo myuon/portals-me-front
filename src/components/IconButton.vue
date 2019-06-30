ce66024a75af912100515ad66d0fd14a81f82e5c
myuon <ioi.joi.koi.loi@gmail.com>
2019-06-30 19:36:05 +0900

    design: icon button

---
+++ src/components/IconButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="icon-button" @click="$emit('click')">
+    <b-icon :icon="icon" size="is-large"/>
+    <div>{{ label }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: ["icon", "label"]
+});
+</script>
+
+<style scoped>
+.icon-button {
+  display: inline-block;
+  text-align: center;
+  padding-top: 12px;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  border: 1px solid #cdcdcd;
+}
+
+.icon-button:hover {
+  background-color: #eee;
+  border-radius: 50%;
+}
+</style>
+
--- src/views/dashboard/CreatePost.vue
+++ src/views/dashboard/CreatePost.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
-    <b-tabs position="is-centered" v-model="activeTab">
-      <b-tab-item label="共有" icon="share">
+    <div style="text-align: center;">
+      <icon-button
+        icon="share"
+        label="共有"
+        slot="trigger"
+        aria-controls="contentIdForAlly1"
+        style="margin: 10px"
+        @click="open('share')"
+      />
+      <icon-button
+        icon="image"
+        label="画像"
+        slot="trigger"
+        aria-controls="contentIdForAlly1"
+        style="margin: 10px"
+        @click="open('image')"
+      />
+    </div>
+
+    <b-collapse :open.sync="isShareOpen" aria-id="contentIdForA11y1">
+      <div class="content">
         <create-share @submit="shareArg = $event; mutate()"/>
-      </b-tab-item>
-      <b-tab-item label="画像" icon="image">
+      </div>
+    </b-collapse>
+
+    <b-collapse :open.sync="isImageOpen" aria-id="contentIdForA11y1">
+      <div class="content">
         <create-image/>
-      </b-tab-item>
-    </b-tabs>
+      </div>
+    </b-collapse>
   </div>
 </template>
 
@@ -18,27 +40,40 @@ import CreateImage from "./CreateImage.vue";
 import * as API from "../../API";
 import * as mutations from "../../graphql/mutations";
 import * as queries from "../../graphql/queries";
+import IconButton from "../../components/IconButton.vue";
 
 export default Vue.extend({
   components: {
     CreateShare,
-    CreateImage
+    CreateImage,
+    IconButton
   },
 
   data() {
     return {
       activeTab: 0,
-      shareArg: null
+      shareArg: null,
+      openTab: null
     };
   },
 
-  computed: {},
+  computed: {
+    isShareOpen() {
+      return this.openTab == "share";
+    },
+    isImageOpen() {
+      return this.openTab == "image";
+    }
+  },
 
   methods: {
     onCreateFinished(type: string) {
       console.log(`${type} created!`);
+    },
+
+    open(tabName: string) {
+      this.openTab = this.openTab == tabName ? null : tabName;
     }
   }
 });
 </script>
-
