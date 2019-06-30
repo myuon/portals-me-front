4d5f07b37ca24ee9b03e8dbbd6eafec085505708
myuon <ioi.joi.koi.loi@gmail.com>
2019-07-01 01:14:58 +0900

    feat: show posts in UserPage

--- src/views/user/UserProfile.vue
+++ src/views/user/UserProfile.vue
@@ -1,22 +1,53 @@
 <template>
-  <div class="one-half is-centered">
-    <figure class="image is-128x128">
-      <img class="is-rounded" :src="user.picture">
-    </figure>
+  <div class="columns is-mobile">
+    <div class="column is-half is-offset-one-quarter">
+      <figure class="image is-128x128">
+        <img class="is-rounded" :src="user.picture">
+      </figure>
 
-    <h2>{{ user.display_name }}</h2>
-    <p>@{{ user.name }}</p>
+      <h2>{{ user.display_name }}</h2>
+      <p>@{{ user.name }}</p>
 
-    <b-button type="is-primary">プロフィールを編集</b-button>
-    <b-button @click="follow" v-if="user.id != me.id">このユーザーをフォロー</b-button>
+      <b-button type="is-primary" v-if="user.id == me.id">プロフィールを編集</b-button>
+      <b-button @click="follow" v-if="user.id != me.id">このユーザーをフォロー</b-button>
+
+      <amplify-connect :query="listPostSummary">
+        <template slot-scope="{ loading, data, errors }">
+          <template v-if="loading">Loading...</template>
+          <template v-else-if="errors.length > 0">{{ errors }}</template>
+          <template v-else-if="data">
+            <list-post :items="data.listPostSummary"/>
+          </template>
+        </template>
+      </amplify-connect>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import ListPost from "../dashboard/ListPost.vue";
+import * as queries from "../../graphql/queries";
+import * as API from "../../API";
 
 export default Vue.extend({
   props: ["user"],
+
+  components: {
+    ListPost
+  },
+
+  computed: {
+    listPostSummary() {
+      console.log(this.$props);
+
+      const arg: API.ListPostSummaryQueryVariables = {
+        owner: this.$props.user.id
+      };
+
+      return this.$Amplify.graphqlOperation(queries.listPostSummary, arg);
+    }
+  },
 
   data() {
     return {
