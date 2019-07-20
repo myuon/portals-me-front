748eb303421f92d196817fb2c5f1852deb380bf8
myuon <ioi.joi.koi.loi@gmail.com>
2019-07-20 22:18:04 +0900

    feat: password-form

--- src/views/PasswordForm.vue
+++ src/views/PasswordForm.vue
@@ -3,11 +3,11 @@
     <div class="columns is-centered">
       <div class="column is-one-quarter">
         <b-field label="user_name">
-          <b-input :value="user_name"/>
+          <b-input v-model="user_name" />
         </b-field>
 
         <b-field label="password">
-          <b-input :value="password"/>
+          <b-input v-model="password" password-reveal />
         </b-field>
 
         <b-button type="is-primary" @click="submit">Submit</b-button>
@@ -29,7 +29,7 @@ export default Vue.extend({
   methods: {
     async submit() {
       const result = await fetch(
-        `${process.env.VUE_APP_AUTH_API_ENDPOINT}/authenticate`,
+        `${process.env.VUE_APP_AUTH_API_ENDPOINT}/signin`,
         {
           method: "POST",
           headers: {
